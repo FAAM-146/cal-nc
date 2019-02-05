a429d825129a4d3011db2d3dee1aa654dc112261
Graeme Nott <graeme.nott@faam.ac.uk>
2019-02-05 17:53:19 +0000

    Added error handling etc to ncgen call. Added checks to return if nothing further to do, checks on number of updates, and correct handling of user and history args

--- cal_ncgen.py
+++ cal_ncgen.py
@@ -30,8 +30,12 @@ def call(infile,args):
 
     :How this works?:
 
-        * open nc -a
-        * determine instrument
+        * .. TODO::
+             concatenate multiple cdl input files
+        * If infile is cdl then create nc by calling ``ncgen``.
+          If no other arguments for nc then return
+        * open nc file
+        * determine instrument to operate on from nc or arg['instr']
         * Instantiate instrument class
         * populate or append to data
         * write nc
@@ -60,7 +64,7 @@ def call(infile,args):
             with open(fname) as infile_:
                 instring = infile_.read()
 
-                # TODO: Remove EOF closing braces and put at end of
+                # .. TODO: Remove EOF closing braces and put at end of
                 # concatenated files
                 outfile.write()
 
@@ -73,19 +77,32 @@ def call(infile,args):
 
         # Run ncgen to produce netCDF-4 format file
         try:
-            subprocess.check_call('ncgen -b -k 2 -o {} {}'.format(outarg,
-                                                            abs_infile[0]),
-                                  shell=True)
+            subprocess.check_call(['ncgen','-b','-k3','-o',
+                                   outarg,abs_infile[0]])
+        except subprocess.CalledProcessError as err:
+            #print('\n',vars(err))
+            if err.returncode == 127:
+                print('\nCommand not found. Check that ncgen is installed.\n')
+            elif err.returncode == 1:
+                # Generally error in cdl
+                print('\nGeneration of netCDF from cdl file failed.')
+                print('Check input cdl syntax.\n')
+            raise SystemExit
         except Exception as err:
-            print('\nSomething when horribly wrong with the ncgen call')
+            print('\nSomething went horribly wrong with the ncgen call\n')
             print('\n',err)
             pdb.set_trace()
 
+    if os.path.splitext(abs_infile[0])[-1].lower() == '.nc' and \
+       os.path.exists(abs_infile[0]):
+        outarg = abs_infile[0]
+
+    if all([v_==None for k_,v_ in args.items() if k_ != 'outfile']):
+        # No arguments given so can return
+        return
+
     # Open nc file for reading/writing
     with netCDF4.Dataset(outarg, mode='r+', format="NETCDF4") as root:
-
-        ########
-        pdb.set_trace()
 
         # Obtain intrument from nc or options
         if args['instr'] is None:
@@ -100,28 +117,71 @@ def call(infile,args):
             # Initialise the nc object
             nc = instr_class(root)
         except:
-            print('Instrument processing class not found: {}'.format(instr))
+            print('Instrument processing class not found: {}\n'.format(instr))
             pdb.set_trace()
 
-        ########
-        pdb.set_trace()
-
-        # Run any additional scripts if necessary
+        # Print out instrument processor help if required
         try:
-            if 'help' in [l[0].lower() for l in args['update_arg']]:
+            if 'help' in args['update_arg']:
                 print(nc)
                 return
         except TypeError:
-            # If args['update_arg'] is None
-            pass
+            # eg if args['update_arg'] is None
+            return
 
-        nc.update(args['update_arg'])
+        # Basic check on update argument, make sure that they are all the
+        # same length. If there are equal number of different lengths, the
+        # first one shall be selected and the other discarded.
+        update_lens = [len(v_) for v_ in args['update_arg']]
+        modal_len = max(set(update_lens),key=update_lens.count) - 1
+
+        # Restructure update args into list of dictionaries.
+        # Note that this does not cope with flattened dictionaries
+        updates = {l_[0]:l_[1:] for l_ in args['update_arg'] if
+                   len(l_) == modal_len + 1}
+
+        # Include some user feedback if any items discarded
+        for u_arg in args['update_arg']:
+            if u_arg[0] not in updates:
+                print('\nUpdate argument discarded as incorrect length')
+                print('  ',u_arg)
+                print()
+
+        # Want to apply history and user attributes for each update so make
+        # sure are correct length.
+        # The username is the same for all updates
+        if args['user'] == None:
+            user = None
+        else:
+            user = [' '.join(args['user'])] * modal_len
+
+        if args['hist'] == None:
+            history = None
+        else:
+            if len(args['hist']) > modal_len:
+                history = args['hist'][:modal_len]
+                discard_hist = args['hist'][modal_len:]
+            elif len(args['hist']) != modal_len:
+                history = [args['hist'][0]] * modal_len
+                discard_hist = args['hist'][1:]
+            else:
+                history = args['hist']
+                discard_hist = []
+
+            if discard_hist != []:
+                print('\nUpdate history argument is incorrect length, '
+                      'discarding entries:')
+                print(' ','\n  '.join(discard_hist))
+                print()
+
+        pdb.set_trace()
+        nc.update(updates)
 
         # Update the history and username attributes
-        nc.update_hist(args['hist'])
-        nc.update_user(args['user'])
+        nc.update_hist(history)
+        nc.update_user(user)
 
-
+        # Save nc back into netCDF4 file
 
     # root closed
     pdb.set_trace()
@@ -136,7 +196,7 @@ if __name__=='__main__':
     import argparse
 
     # Define commandline options
-    usage = '%(prog)s filename [options]'
+    usage = '%(prog)s files [options]'
     version = 'version: {v}'.format(v=cal_proc.__version__)
     description = ('Script to assist in the creation of calibration ' +\
                    'netCDF files.\n {0}'.format(version))
@@ -167,8 +227,8 @@ if __name__=='__main__':
                         help=('Space-delineated list of data parameters '
                         'as required by the specific instrument class. '
                         "'--update' can be given multiple times, each time "
-                        'used to update and different parameter '
-                        "Use '-d help' for instrument specific help."))
+                        'used to update a different parameter.'
+                        "Use '-u help' for instrument specific help."))
     parser.add_argument('-i', '--instr', action='store',
                         dest='instr', default=None,
                         help=('Instrument class is selected based on the '
@@ -186,29 +246,24 @@ if __name__=='__main__':
                         'created.'))
 
     parser.add_argument('--hist', action='store',
-                        dest='hist', nargs='?',
+                        dest='hist', nargs='*',
                         default=None,
-                        const='NA',
                         help=('Do not auto-generate the updated history '
-                        'attribute of the resultant cal nc file. If a string '
-                        'is given then this string is appended to the '
-                        'history attribute instead (should be space-seperated '
-                        'date and comment). If no string is given '
-                        'then assume history updates have been handled in '
-                        'the cdl file/s. This option should be used with '
-                        'care.'))
+                        'attribute of the resultant cal nc file. Instead the '
+                        'given string is appended to the global history '
+                        'attribute instead. These should be a space-delineated '
+                        'series of dates and comments, spaces in each entry '
+                        'must be enclosed in quotes or escaped. There should '
+                        'be one history entry for each --update, if not then '
+                        'the same history shall be attached to all updates.'))
     parser.add_argument('--user', action='store',
-                        dest='user', nargs='?',
+                        dest='user', nargs='*',
                         default=None,
-                        const='NA',
                         help=('Do not auto-generate the updated username '
-                        'attribute of the resultant cal nc file. If a string '
-                        'is given then this string is appended to the '
-                        'username attribute instead (should be space-seperated '
-                        'user and <email>). If no string is given '
-                        'then assume username updates have been handled in '
-                        'the cdl file/s. This option should be used with '
-                        'care.'))
+                        'attribute of the resultant cal nc file. Instead the '
+                        'given string is appended to the global username '
+                        'attribute instead (should be space-seperated '
+                        'user and <email>).'))
     parser.add_argument('-v', '--version', action='version',
                         version=version,
                         help='Display program version number.')
@@ -224,7 +279,6 @@ if __name__=='__main__':
     print('\t  Calibration netCDF generation script')
     print('-----------------------------------------------------\n')
 
-    pdb.set_trace()
     call(args_dict['files'],opts_dict)
 
     print('\nDone.\n')

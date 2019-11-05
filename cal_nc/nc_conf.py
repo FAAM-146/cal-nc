badd298c3f4a67fbe05d6ec67e52be58d3886b85
Graeme Nott <graeme.nott@faam.ac.uk>
2019-11-05 16:55:26 +0000

    Added extract_specials() to extract special options from config dictionary. Improved read_config() to convert option value types

--- cal_nc/nc_conf.py
+++ cal_nc/nc_conf.py
@@ -29,12 +29,16 @@ __author_email__ = 'graeme.nott@faam.ac.uk'
 __copyright__ = '2019, FAAM'
 
 
-__all__ = ['read_config']
+__all__ = ['read_config','extract_specials']
 
 
 # Directory where temporary files are stored
 default_tmp_dir = './tmp'
 
+
+# List of default 'special key strings' in configuration dictionary
+default_special_str = ['_group',
+                       '_parsefile']
 
 
 def read_config(cfg_file,destr=False):
@@ -52,34 +56,60 @@ def read_config(cfg_file,destr=False):
                     returns None if cfg_file cannot be found
     """
 
+    # Manual configuration converters
+    def list_converter(s):
+        """
+        Convert comma-delineated string to list of numbers or strings as
+        appropriate. 'none' strings are converted to None
+        """
+        assert ',' in s
+        l = [s_.strip().replace('"','').replace('\'','') for s_ in s.split(',')]
 
-    def _ConfigSectionMap(section):
+        for conv_ in (int,float,str):
+            try:
+                l = [None if s_.lower()=='none' else conv_(s_) for s_ in l[:]]
+            except ValueError as err:
+                continue
+            else:
+                break
+        return l
+
+    def _ConfigSectionMap(section,rtn_str=False):
         # Read each option within the given section
-        dict1 = {}
+        # If rtn_str is False then attempt to inteprete/convert string
+        if rtn_str == True:
+            getter = [cfg.get]
+        else:
+            getter = [cfg.getlist,
+                      cfg.getint,cfg.getfloat,
+                      cfg.getboolean,   # after get numbers so 0/1 are ints
+                      cfg.get]
+
+        cfg_d = {}
         options = cfg.options(section)
         for option in options:
-            try:
-                dict1[option] = cfg.get(section, option)
-                if dict1[option] == -1:
-                    DebugPrint('skip: {}'.format(option))
-            except:
-                print('exception on {}!'.format(option))
-                dict1[option] = None
-        return dict1
+            for getter_ in getter:
+                try:
+                    cfg_d[option] = getter_(section, option)
+                except ValueError as err:
+                    continue
+                except AssertionError as err:
+                    # String does not include comma so not a list
+                    # Raised by list_converter()
+                    continue
+                except Exception as err:
+                    print('exception on {}!'.format(option))
+                    cfg_d[option] = None
+                    break
+                else:
+                    break
 
-    def _destr_dict(d):
-        # Walk through multi-layer dictionary and convert string values to
-        # other types as appropriate
-        for k, v in d.iteritems():
-            if isinstance(v, dict):
-                _destr_dict(v)
-            else:
-                if v.lower() == 'none':
-                    d[k] = None
-                elif v.lower() == 'true':
-                    d[k] = True
-                elif v.lower() == 'false':
-                    d[k] = False
+            # Attempt to 'none' strings to None
+            if cfg_d[option] in ['none', 'None', 'NONE']:
+                cfg_d[option] = None
+
+        return cfg_d
+
 
     # Check file validity
     if not os.path.exists(cfg_file):
@@ -90,15 +120,14 @@ def read_config(cfg_file,destr=False):
             print('filename {} does not exist.'.format(cfg_file))
             return None
 
-    cfg = configparser.ConfigParser(allow_no_value=True)
+
+    cfg = configparser.ConfigParser(allow_no_value=True,
+                                    converters={'list': list_converter})
 
     cfg.read(cfg_file)
     cfg_dict = {}
     for l in cfg.sections():
         cfg_dict[l] = _ConfigSectionMap(l)
-
-    if destr is True:
-        _destr_dict(cfg_dict)
 
     if cfg_dict == {}:
         print('\ncfg_dict is empty but ' +\
@@ -108,3 +137,37 @@ def read_config(cfg_file,destr=False):
         cfg_dict = None
 
     return cfg_dict
+
+
+def extract_specials(cfg_dict,specials = [],overwrite=False):
+    """
+    Extract special configuration keys and return
+
+    :param cfg_dict: Configuration dictionary
+    :type cfg_dict: dictionary
+    :param specials: list of special strings to extract if they are present.
+        Default is default_special_str. Any special strings given are appended
+        to default_special_str rather than replacing it if overwrite is False.
+    :type specials: list
+    :params overwrite: If False [default] then any specials given in kwargs
+        are appended to default_special_str. If True then only those strings
+        included in specials are searched for.
+    :type overwrite: boolean
+    :returns: Dictionary of specials with keys of the special strings and
+        input cfg_dict with special items removed. If no specials found then
+        specials_dict will be {}.
+    """
+    #pdb.set_trace()
+    if type(specials) in [str]:
+        specials = [specials]
+
+    if overwrite == False or len(specials) == 0:
+        specials.extend(default_special_str)
+
+    s_l = [s_.lower() for s_ in specials]
+
+    specials_dict =  {k_:v_ for (k_,v_) in cfg_dict.items() if k_ in s_l}
+    for k_ in specials_dict.keys():
+        _ = cfg_dict.pop(k_)
+
+    return cfg_dict, specials_dict
